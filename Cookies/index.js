e8213cfd7e64c932eb0e57b863a1f8e68f212467
regal-rahul <regalrahul777@gmail.com>
2022-06-13 13:29:49 +0530

    Passing signed cookies and printing it

--- Cookies/index.js
+++ Cookies/index.js
@@ -18,6 +18,12 @@ app.get('/setname', (req, res) => {
 
 app.get('/getsignedcookies', (req, res) => {
     res.cookie('fruit', 'grape', { signed: true });
+    res.send('Signed your fruit cookie.')
+})
+app.get('/verifyfruit', (req, res) => {
+    console.log(req.cookies);
+    console.log(req.signedCookies);
+    res.send(req.signedCookies);
 })
 
 app.listen(3000, () => {
