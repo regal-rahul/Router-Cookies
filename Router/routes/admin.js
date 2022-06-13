f78c002cea05498dc5eed354c296f1ae32e07259
regal-rahul <regalrahul777@gmail.com>
2022-06-13 12:01:13 +0530

    Add adminRoutes

--- Router/routes/admin.js
+++ Router/routes/admin.js
@@ -2,6 +2,12 @@ const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
 
+router.use((req, res, next) => {
+    if (req.query.isAdmin) {
+        next();
+    }
+    res.send('Sorry, Not an Admin!');
+})
 
 router.get('/topsecret', (req, res) => {
     res.send('This is top secret.');
